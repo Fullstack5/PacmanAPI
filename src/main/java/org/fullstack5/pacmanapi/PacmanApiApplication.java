e31a5e8c0838c2158a19ae0618946f1b40f03e24
qc53bf <jesse.brand@ing.nl>
2018-05-02 10:46:45 +0200

    Initiate project.

---
+++ src/main/java/org/fullstack5/pacmanapi/PacmanApiApplication.java
@@ -0,0 +1,12 @@
+package org.fullstack5.pacmanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmanApiApplication.class, args);
+	}
+}
---
+++ src/test/java/org/fullstack5/pacmanapi/PacmanApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fullstack5.pacmanapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PacmanApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
