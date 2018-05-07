d388a4dc14a911107983dd29afa4471b76d5f737
qc53bf <jesse.brand@ing.nl>
2018-05-07 14:03:18 +0200

    Immutability.

--- src/main/java/org/fullstack5/pacmanapi/models/GameState.java
+++ src/main/java/org/fullstack5/pacmanapi/models/GameState.java
@@ -3,10 +3,12 @@ package org.fullstack5.pacmanapi.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Immutable instance of the game state in time.
+ */
 @Getter
 @AllArgsConstructor
-public class GameState {
-    // this class is meant to be immutable
-    private long time;
-    private Position pacman;
+public final class GameState {
+    private final long time;
+    private final Position pacman;
 }
