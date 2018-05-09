e449dc502584a5dcf2abd6e15af65224a5072c5f
qc53bf <jesse.brand@ing.nl>
2018-05-09 09:46:53 +0200

    Some configuration per direction.

--- src/main/java/org/fullstack5/pacmanapi/models/Direction.java
+++ src/main/java/org/fullstack5/pacmanapi/models/Direction.java
@@ -1,8 +1,28 @@
 package org.fullstack5.pacmanapi.models;
 
+import lombok.Getter;
+
 /**
  * Direction to move in.
  */
+@Getter
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST
+    NORTH(90, 0, -1),
+    EAST(0, 1, 0),
+    SOUTH(270, 0, 1),
+    WEST(180, -1, 0);
+
+    private final int angle;
+    private final int xInc;
+    private final int yInc;
+
+    private Direction(final int angle, final int xInc, final int yInc) {
+        this.angle = angle;
+        this.xInc = xInc;
+        this.yInc = yInc;
+    }
+
+    public static Direction random() {
+        return Direction.values()[(int) (Math.random() * Direction.values().length)];
+    }
 }
