96d371ebe712599f2bbc50ad8691f0452dbb03c9
qc53bf <jesse.brand@ing.nl>
2018-05-09 12:38:07 +0200

    Don't run headless.

--- src/main/java/org/fullstack5/pacmanapi/PacmanApiApplication.java
+++ src/main/java/org/fullstack5/pacmanapi/PacmanApiApplication.java
@@ -2,11 +2,12 @@ package org.fullstack5.pacmanapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class PacmanApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PacmanApiApplication.class, args);
+		new SpringApplicationBuilder(PacmanApiApplication.class).headless(false).run(args);
 	}
 }
