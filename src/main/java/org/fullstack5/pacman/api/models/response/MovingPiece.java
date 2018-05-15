b5f6f666f92adb8e250be20337dde836bf3dad63
qc53bf <jesse.brand@ing.nl>
2018-05-15 15:28:35 +0200

    Fix deserializing trouble with models.

--- src/main/java/org/fullstack5/pacman/api/models/response/MovingPiece.java
+++ src/main/java/org/fullstack5/pacman/api/models/response/MovingPiece.java
@@ -2,6 +2,7 @@ package org.fullstack5.pacman.api.models.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.fullstack5.pacman.api.models.Direction;
 import org.fullstack5.pacman.api.models.Position;
 
@@ -10,9 +11,10 @@ import org.fullstack5.pacman.api.models.Position;
  */
 @Getter
 @AllArgsConstructor
-public class MovingPiece {
-    final Position oldPosition;
-    final Position currentPosition;
-    final Direction direction;
-    final boolean vulnerable;
+@NoArgsConstructor
+public final class MovingPiece {
+    private Position oldPosition;
+    private Position currentPosition;
+    private Direction direction;
+    private boolean vulnerable;
 }
