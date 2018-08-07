41306763e52a0b924699945059134577e18cf119
qc53bf <jesse.brand@ing.nl>
2018-08-07 14:50:39 +0200

    Utility inverse method.

--- src/main/java/org/fullstack5/pacman/api/models/Direction.java
+++ src/main/java/org/fullstack5/pacman/api/models/Direction.java
@@ -25,4 +25,18 @@ public enum Direction {
     public static Direction random() {
         return Direction.values()[(int) (Math.random() * Direction.values().length)];
     }
+
+    public static Direction reverse(final Direction direction) {
+        switch(direction) {
+        case NORTH:
+            return SOUTH;
+        case EAST:
+            return WEST;
+        case SOUTH:
+            return NORTH;
+        case WEST:
+            return EAST;
+        }
+        return null;
+    }
 }
